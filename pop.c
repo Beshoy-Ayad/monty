a5c0573cb49f711036b228546ea81720bfd9529f
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-06-23 15:54:35 -0700

    0.6

---
+++ pop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * pop - remove the top element of the stack.
+ *
+ * @stack: Pointer to the stack.
+ * @line_number: Line number of the instruction.
+ *
+ * Return: void.
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	node = *stack;
+	*stack = (*stack)->next;
+	free(node);
+}

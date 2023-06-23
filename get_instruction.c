6b7eaff8f69fdda4a4be6217b747fa9bdd3f5db1
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-06-23 15:39:28 -0700

    0.3

---
+++ get_instruction.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * get_instruction - function that call right function
+ * corresponding to an opcode.
+ *
+ * @opcode: string command.
+ *
+ * Return: A pointer to the instruction_t struct or NULL.
+ */
+instruction_t *get_instruction(char *opcode)
+{
+	int i;
+
+	static instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", div_op},
+		{"mul", mul_op},
+		{"mod", mod_op},
+		{"pchar", pchar_op},
+		{NULL, NULL}};
+	for (i = 0; instructions[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+			return (&instructions[i]);
+	}
+	return (NULL);
+}
